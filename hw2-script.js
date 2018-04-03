1de7273d18c1f62e9261a8bb02bd8b698eac6fd6
nunuchi <36165551+nunuchi@users.noreply.github.com>
2018-04-03 14:02:12 -0500

    Add files via upload

--- hw2-script.js
+++ hw2-script.js
@@ -1,73 +1,107 @@
-var app = angular.module('myApp', []);
+  var app = angular.module('myApp', []);
 app.controller('aqCtrl', function($scope, $http) {
   $http.get("https://api.openaq.org/v1/measurements").then(function (response) {
       $scope.myData = response.data.results;
   });
 });
-
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: 'roadmap'
-  });
-
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
+  function initMap(){
+    //The map options
+    var options = {
+      zoom: 8,
+      center: {lat: -33.8688, lng: 151.2195}
     }
+    //Create new map
+    var map = new google.maps.Map(document.getElementById('map'), options);
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
+
+    //add a listener, that changes the user_input to the center of where they are on the map
+    map.addListener('center_changed', function() {
+    var center = map.getCenter();
+    var latitude = center.lat();
+    var longitude = center.lng();
+    document.getElementById("user_input").value = latitude+','+longitude;
     });
-    markers = [];
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
+
+    // var input = document.getElementById('user_input').value;
+    // input = input.split(',');
+    // alert(input[0]+','+input[1]);
+
+
+    var input = document.getElementById('user_input');
+    input.addEventListener('keyup', function(event) {
+      var input2 = input.value;
+      var inputSplit = input2.split(',');
+
+      if (event.keyCode === 13) {
+        var latLng = new google.maps.LatLng(inputSplit[0], inputSplit[1])
+        map.panTo(latLng);
+        alert(inputSplit[0]+','+inputSplit[1]);
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+    });
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
+
+    map.setCenter({lat:75,lng:150});
+
+    //https://api.openaq.org/v1/measurements?coordinates=28.63576,77.22445&radius=2500, example of coor + radius
+
+    //Array of markers
+    var markersArray = 
+    [
+      { 
+        coord:{lat: -33.8688, lng: 151.2195}, 
+        iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        content:'<h1>City, State</h1>'
+      },
+      { 
+        coord:{lat: -36, lng: 150},
+        content:'<h1>City, State</h1>'
+      },
+      { 
+        coord:{lat: -27, lng: 150},
+        content:'<h1>City, State</h1>'
+      },
+    ];
+
+    for(var i = 0; i < markersArray.length; i++) {
+      addMarker(markersArray[i]);
+    }//for(var i = 0; i < markersArray.length; i++)
+
+
+    /*addMarker({coord:{lat: -32, lng: 150}});
+    addMarker({ coord:{lat: -33, lng: 150}, 
+          iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          content:'<h1>City, State</h1>'
+          });*/
+
+
+  //Map Markers, have to insert positions into the markers
+  //position: {lat: x, lng: y}, where to place the marker
+  //map:map, which map to set the marker
+  //icon: 'https.png', to set icons for different markers
+    function addMarker(property){
+      var marker = new google.maps.Marker({
+        position: property.coord,
         map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
+      });
+
+      //Check for if Icon value exists
+      if(property.iconImage){ //Sets IconImage of the Marker
+        marker.setIcon(property.iconImage);
+      }//if(property.iconImage)
+
+      //Check for if Content value exists
+      if(property.content){//Sets the Content to the Marker
+        var infoWindow = new google.maps.InfoWindow({
+          content:property.content
+        });
+
+        //adds a click listener to the Marker, showing the Content
+        marker.addListener('click', function(){
+          infoWindow.open(map, marker);
+        });
+
+      }//if(property.content)
+    }//function addMarker(property)
+  }//function initMap()
